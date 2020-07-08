61e21d059e4866489907e18efb626cf04f77a52d
noahcote <noah.cote3@gmail.com>
2020-07-08 13:48:04 -0400

    first

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 from tqdm import tqdm
 import json
-from statistics import mean, median, mode
 
 def landlord_count(source, output):
     landlords = {}
@@ -27,9 +26,6 @@ def landlord_count(source, output):
     print("There are ", line_count, "properties in this data set.")
     print("There are ", landlord_count, "unique owner_1's.")
     print("That's ", line_count/landlord_count, "properties per landlord!")
-    print("Mean Properties Owned: ", mean(landlords.values()))
-    print("Median Properties Owned: ", median(landlords.values()))
-    print("Mode Properties Owned: ", mode(landlords.values()))
 
     sorted_landlords = sorted(landlords.items(), key=lambda x: x[1], reverse=True)
     with open(output, 'w') as file:
@@ -61,8 +57,26 @@ def json_creator(source, output):
         with open(output, 'w') as file:
             file.write(json.dumps(data))
 
+def remove_one_off_landlords(source, output, significant_property_count):
+    with open(source, mode='r') as file:
+        data = file.read()
+    landlords_and_properties = json.loads(data)
+    landlord_count = len(landlords_and_properties.keys())
+    significant_landlords = {}
+    for landlord in tqdm(landlords_and_properties, total=landlord_count):
+        if landlords_and_properties[landlord]['total_properties'] >= significant_property_count:
+            significant_landlords[landlord] = {
+                'total_properties': landlords_and_properties[landlord]['total_properties'],
+                'properties': landlords_and_properties[landlord]['properties']
+            }
+    print('Significance Threshold: ', significant_property_count, 'Properties Owned')
+    print('Significant Landlords: ', len(significant_landlords))
+    with open(output, 'w') as file:
+            file.write(json.dumps(significant_landlords))
+
 def main():
 #     landlord_count('opa_properties_public.csv', 'unique_landlords.json')
-    json_creator('opa_properties_public.csv', 'landlords_and_properties.json')
+#     json_creator('opa_properties_public.csv', 'landlords_and_properties.json')
+    remove_one_off_landlords('landlords_and_properties.json', 'significant_landlords.json', 14)
 
 main()
