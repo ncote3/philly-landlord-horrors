16f42e179f38eb0a581c440a1f8cfc052a680812
noahcote <noah.cote3@gmail.com>
2020-07-09 12:25:38 -0400

    added more documentation and more json generators

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
 # philly-landlord-horrors
-Playing with Philly Open Property Assessment Data to uncover the secrets of the owning class
+Playing with Philly Open Property Assessment Data to uncover the secrets of the owning class.
 
 ## Data Source:
 https://www.opendataphilly.org/dataset/opa-property-assessments
+
+## To Run:
+
+Make sure you have downloaded the data set and place it in the
+root of this repository. The program knows it as `opa_properties_public.csv`.
+
+This program uses Python3.
+
+The following needs to be installed in your python3 venv:
+* `tqdm`
+* `numpy`
+* `matplotlib`
+
+Once these are installed simply run: `python3 main.py`
+
+## Usage:
+###`landlord_count(source, output)`
+This function creates a basic dictionary histogram of owner_1's and how many properties they own. 
+
+It saves the results to a json file.
+
+###`json_creator(source, output)`
+This function creates a dictionary using owner_1's as a key and for the value a dictionary
+of how many properties they own, a list of the properties, and the properties geo coordinates.
+
+It saves the results to a json file.
+
+###`landlord_json_creator(source, output)`
+This function creates a basic dictionary of owner_1's and how many properties they own.
+
+It saves the results to a json file.
+
+###`property_json_creator(source, output)`
+This function creates a basic list of the properties in the data set.
+
+It saves the results to a json file.
+
+###`remove_one_off_landlords(source, output, significant_property_count)`
+This function modifies a json file to contain only landlords with property counts higher than
+significant_property_count.
+
+The results are saved to a json file.
+
+###`histogram(source, n_bins)`
+This function takes json landlord histogram data and turns it into a
+logged histogram to visualize gaps in ownership
+
+###`housing_justice_node_json_generator()`
+This function generates the json files needed to easier process data in other programs.
+
+###`main()`
+Program driver, insert the function you want to use here.
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# This program is meant to take data provided by Philadelphia's public property data and modify it.
+# In order for this to work it is preferred to as opa_properties_public.csv
+# Here is a link to the data: https://www.opendataphilly.org/dataset/opa-property-assessments
+
 import csv
 from tqdm import tqdm
 import json
 import matplotlib.pyplot as plt
 import numpy as np
 
+# void landlord_count(String source, String output)
+# This function creates a basic dictionary histogram of owner_1's
+# and how many properties they own.
+# It saves the results to a json file.
 def landlord_count(source, output):
     landlords = {}
 
@@ -33,6 +41,10 @@ def landlord_count(source, output):
     with open(output, 'w') as file:
         file.write(json.dumps(sorted_landlords))
 
+# void json_creator(String source, String output)
+# This function creates a dictionary using owner_1's as a key and for the value a dictionary
+# of how many properties they own, a list of the properties, and the properties geo coordinates.
+# It saves the results to a json file.
 def json_creator(source, output):
     data = {}
 
@@ -60,9 +72,55 @@ def json_creator(source, output):
                 data[row['owner_1'].strip()]['prop_coords'] = prop_coords
 
                 line_count += 1
-        with open(output, 'w') as file:
-            file.write(json.dumps(data))
+    with open(output, 'w') as file:
+        file.write(json.dumps(data))
 
+# void landlord_json_creator(String source, String output)
+# This function creates a basic dictionary of owner_1's and how many properties they own.
+# It saves the results to a json file.
+def landlord_json_creator(source, output):
+    landlords = {}
+
+    with open(source, mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in tqdm(csv_reader, total=581456):
+            if line_count == 0:
+                line_count += 1
+            if row["owner_1"] not in landlords.keys():
+                landlords[row["owner_1"].strip()] = 1
+                line_count += 1
+            else:
+                try:
+                    landlord_prop_count = landlords[row["owner_1"].strip()]
+                    landlords[row["owner_1"].strip()] = landlord_prop_count + 1
+                except:
+                    print(row["owner_1"].strip(), "is missing a count.")
+                line_count += 1
+    with open(output, 'w') as file:
+        file.write(json.dumps(landlords))
+
+# void property_json_creator(String source, String output)
+# This function creates a basic list of the properties in the data set.
+# It saves the results to a json file.
+def property_json_creator(source, output):
+    properties = []
+
+    with open(source, mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in tqdm(csv_reader, total=581456):
+            if line_count == 0:
+                line_count += 1
+            properties.append(row['location'].strip())
+            line_count += 1
+    with open(output, 'w') as file:
+        file.write(json.dumps(properties))
+
+# void remove_one_off_landlords(String source, String output, int significant_property_count)
+# This function modifies a json file to contain only landlords with property counts higher than
+# significant_property_count.
+# The results are saved to a json file.
 def remove_one_off_landlords(source, output, significant_property_count):
     with open(source, mode='r') as file:
         data = file.read()
@@ -80,6 +138,9 @@ def remove_one_off_landlords(source, output, significant_property_count):
     with open(output, 'w') as file:
             file.write(json.dumps(significant_landlords))
 
+# void histogram(String source, int n_bins)
+# This function takes json landlord histogram data and turns it into a
+# logged histogram to visualize gaps in ownership
 def histogram(source, n_bins):
     with open(source, mode='r') as file:
         landlords_and_properties = file.read()
@@ -92,10 +153,23 @@ def histogram(source, n_bins):
     plt.hist(x, n_bins, log=True)
     plt.show()
 
+# void housing_justice_node_json_generator()
+# This function generates the json files needed to easier process data in other programs.
+def housing_justice_node_json_generator():
+    print('Creating landlords_and_properties.json...')
+    json_creator('opa_properties_public.csv', 'landlords_and_properties.json')
+    print('Creating properties.json...')
+    property_json_creator('opa_properties_public.csv', 'properties.json')
+    print('Creating landlords.json...')
+    landlord_json_creator('opa_properties_public.csv', 'landlords.json')
+
 def main():
 #     landlord_count('opa_properties_public.csv', 'unique_landlords.json')
-    json_creator('opa_properties_public.csv', 'landlords_and_properties.json')
+#     json_creator('opa_properties_public.csv', 'landlords_and_properties.json')
 #     remove_one_off_landlords('landlords_and_properties.json', 'significant_landlords.json', 1)
 #     histogram('significant_landlords.json', 100)
+#     landlord_json_creator('opa_properties_public.csv', 'landlords.json')
+#     property_json_creator('opa_properties_public.csv', 'properties.json')
+    housing_justice_node_json_generator()
 
 main()
